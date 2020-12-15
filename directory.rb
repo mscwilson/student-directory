d37660c682966a76d951ea6c7eef1e376bef95a6
Miranda Wilson <mirandascwilson@gmail.com>
2020-12-15 11:09:45 +0000

    Print_names now takes uer input

--- directory.rb
+++ directory.rb
@@ -17,11 +17,20 @@ def print_header
 end
 
 def print_names(students)
-  students.each { |student| puts "#{student[:name]} (#{student[:cohort]} cohort)"}
+  puts "Print out all names or a subset? You can print all names starting with a certain letter, or all names shorter than a particular number."
+  puts "Choose 'all', or type a letter, or a number"
+  input = gets.chomp
+  if input == "all"
+    students.each_with_index { |student, i| puts "#{i + 1}. #{student[:name]} (#{student[:cohort]} cohort)"}
+  elsif input.to_i.is_a?(Integer) && input.to_i != 0
+    students.each { |student| puts "#{student[:name]} (#{student[:cohort]} cohort)" if student[:name].length < input.to_i }
+  elsif input.is_a?(String) && input.length == 1
+    students.each { |student| puts "#{student[:name]} (#{student[:cohort]} cohort)" if student[:name][0].downcase == input.downcase }
+  end
 end
 
 def print_footer(students)
-  puts "Overall, we have #{students.length} great students."
+  puts "In total, we have #{students.length} great students."
 end
 
 students = input_students
