bea32d0bc0bf3680bb279af5bec8687d9b50404e
Miranda Wilson <mirandascwilson@gmail.com>
2020-12-16 14:13:56 +0000

    Added writing student list functionality and .gitignore

--- directory.rb
+++ directory.rb
@@ -85,18 +85,17 @@ def print_footer
 end
 
 def interactive_menu
-  
   loop do
     print_menu
     process(gets.chomp)
   end
-
 end
 
 def print_menu
   puts "What would you like to do?"
   puts "1. Input the students"
   puts "2. Show the students"
+  puts "3. Save the student list"
   puts "10. Exit"
 end
 
@@ -106,6 +105,8 @@ def process(selection)
     input_students
   when "2"
     show_students
+  when "3"
+    save_students
   when "10"
     exit
   else
@@ -117,6 +118,16 @@ def show_students
   print_header
   print_student_names
   print_footer
+end
+
+def save_students
+  file = File.open("students.csv", "w")
+  @students.each do |student|
+    student_data = [student[:name], student[:cohort]]
+    csv_line = student_data.join(",")
+    file.puts csv_line
+  end
+  file.close
 end
 
 interactive_menu
