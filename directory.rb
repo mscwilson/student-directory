c3e29be50342129b7a000319f86d0b6b1ad3a543
Miranda Wilson <mirandascwilson@gmail.com>
2020-12-16 13:54:12 +0000

    students is now global variable

--- directory.rb
+++ directory.rb
@@ -1,4 +1,4 @@
-
+@students = []
 
 # Currently not using any of the other default student info aside from cohort
 def create_student(full_name, cohort = Time.now.strftime("%B").to_sym, height_cm = 165, country_of_origin = "UK", hobbies = ["coding"], no_of_pets = 0)
@@ -10,7 +10,7 @@ def create_student(full_name, cohort = Time.now.strftime("%B").to_sym, height_cm
   no_of_pets: no_of_pets}
 end
 
-def input_students(students)
+def input_students
   puts "Please enter the names of the students.\nYou can optionally specify the cohort if it's not the current month - separate the cohort from the name with a comma. Full or abbreviated month names are both fine."
   puts "To finish, just hit return twice"
   full_input = gets.delete!("\n")
@@ -22,22 +22,22 @@ def input_students(students)
   while !full_input.empty? do
     full_name, cohort = full_input.split(", ")
     if cohort != nil && months.include?(cohort.capitalize.to_sym)
-      students << create_student(full_name, cohort.capitalize)
+      @students << create_student(full_name, cohort.capitalize)
     elsif cohort != nil && months.any? { |k, v| v.include?(cohort.capitalize) }
       unabbreviated_month = months.find { |k, v| v.include?(cohort.capitalize)}
-      students << create_student(full_name, unabbreviated_month[0])
+      @students << create_student(full_name, unabbreviated_month[0])
     else
-      students << create_student(full_name)
+      @students << create_student(full_name)
     end
 
-    if students.length == 1
-      puts "Now we have #{students.length} student"
+    if @students.length == 1
+      puts "Now we have #{@students.length} student"
     else
-      puts "Now we have #{students.length} students"
+      puts "Now we have #{@students.length} students"
     end
     full_input = gets.chomp
   end
-  students
+  @students
 end
 
 def print_header
@@ -45,61 +45,56 @@ def print_header
   puts "-------------".center(100)
 end
 
-def print_names(students)
-  return if students.empty?
+def print_names
+  return if @students.empty?
 
   puts "Print out all names or a subset? You can print by cohort, or all names starting with a certain letter, or all names shorter than a particular number."
   puts "Choose 'all', 'cohort', or type a letter or a number"
   input = gets.delete!("\n")
   if input == "all"
     counter = 0
-    until counter == students.length
+    until counter == @students.length
       print "#{counter + 1}.".center(4)
-      puts "#{students[counter][:name]} (#{students[counter][:cohort]} cohort)"
+      puts "#{@students[counter][:name]} (#{@students[counter][:cohort]} cohort)"
       counter += 1
     end
 
   elsif input == "cohort"
-    students.each_with_object(cohorts_list = []) { |dict, arr| arr << dict[:cohort]}
+    @students.each_with_object(cohorts_list = []) { |dict, arr| arr << dict[:cohort]}
     cohorts_list.uniq!.each do |month|
       puts "#{month} cohort:"
-      students.each { |student| puts "- #{student[:name]}" if student[:cohort] == month }
+      @students.each { |student| puts "- #{student[:name]}" if student[:cohort] == month }
       puts
     end
 
   elsif input.to_i.is_a?(Integer) && input.to_i != 0
-    students.each { |student| puts "#{student[:name]} (#{student[:cohort]} cohort)" if student[:name].length < input.to_i }
+    @students.each { |student| puts "#{student[:name]} (#{student[:cohort]} cohort)" if student[:name].length < input.to_i }
   elsif input.is_a?(String) && input.length == 1
-    students.each { |student| puts "#{student[:name]} (#{student[:cohort]} cohort)" if student[:name][0].downcase == input.downcase }
+    @students.each { |student| puts "#{student[:name]} (#{student[:cohort]} cohort)" if student[:name][0].downcase == input.downcase }
   end
 end
 
-def print_footer(students)
-  if students.empty?
+def print_footer
+  if @students.empty?
     puts "No students are enrolled."
-  elsif students.length == 1
-    puts "In total, we have #{students.length} great student."
+  elsif @students.length == 1
+    puts "In total, we have #{@students.length} great student."
   else
-    puts "In total, we have #{students.length} great students."
+    puts "In total, we have #{@students.length} great students."
   end
 end
 
 def interactive_menu
-  students = []
+  
   loop do
-    puts "What would you like to do?"
-    puts "1. Input the students"
-    puts "2. Show the students"
-    puts "10. Exit"
+    print_menu
     selection = gets.chomp
 
     case selection
     when "1"
-      input_students(students)
+      input_students
     when "2"
-      print_header
-      print_names(students)
-      print_footer(students)
+      show_students
     when "10"
       exit
     else
@@ -109,6 +104,18 @@ def interactive_menu
 
 end
 
+def print_menu
+  puts "What would you like to do?"
+  puts "1. Input the students"
+  puts "2. Show the students"
+  puts "10. Exit"
+end
+
+def show_students
+  print_header
+  print_names
+  print_footer
+end
 
 
 # students = input_students
